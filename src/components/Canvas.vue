d7572a94521b88745983ebbb29dd75b873819a6b
DaveBruwer <davebruwer@gmail.com>
2022-11-04 22:44:00 +1300

    code refactoring...

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -34,6 +34,9 @@ export default {
                 top: 0,
                 left: 0,
                 alive: false,
+                nextAlive: Math.random() > 0.5 ? true : false,
+                crowd: 0,
+
             });
         }
 
@@ -42,9 +45,6 @@ export default {
         this.setCanvasSize();
 
         window.addEventListener("resize", this.setCanvasSize);
-
-        this.gridUpdate();
-        
     },
     methods: {
         setCanvasSize() {
@@ -57,35 +57,26 @@ export default {
             this.grid.left = 0 + (this.canvas.width - this.grid.gridSize)/2;
             this.grid.top = 0 + (this.canvas.height - this.grid.gridSize)/2;
 
+            this.gridInit();
         },
         gridInit() {
         // Determines the coordinates of each cell in the grid.
             this.grid.cellSize = this.grid.gridSize / Math.sqrt(this.grid.array.length);
 
-            
             this.grid.array.forEach((cell, i) => {
                 cell.row = i%Math.sqrt(this.grid.array.length);
                 cell.col = Math.floor(i/Math.sqrt(this.grid.array.length));
                 
                 cell.left = this.grid.left + this.grid.cellSize * (cell.row);
                 cell.top = this.grid.top + this.grid.cellSize * (cell.col);
-
-                // Randomly turns cells on. Will take this out later.
-                if (this.grid.init) {
-                    if (Math.random() > 0.5) {
-                        this.grid.array[i].nextAlive = true;
-                    }
-                    this.grid.init = true;
-                }
             });
+
+            this.lifeUpdate();
         },
         gridUpdate() {
         // Turns cells "on" or "off" based on their .nextAlive property and then updates the .alive property to reflect current state.
 
-            // Initialize the grid if not yet done.
-            this.gridInit();
-
-            for (let cell of this.grid.array) {
+            this.grid.array.forEach((cell, i) => {
 
                 // Clear all pixels on cell.
                 this.ctx.clearRect(
@@ -125,49 +116,87 @@ export default {
                     this.grid.cellSize,
                     this.grid.cellSize
                 );
-            }
+                
+                // if (cell.alive) {
+                //     this.ctx.strokeText("a", cell.left + 7, cell.top+15);
+                // } else {
+                //     this.ctx.strokeText("", cell.left + 7, cell.top+15);
+                // }
+
+                // this.ctx.strokeText(cell.crowd, cell.left + 7, cell.top+15);
+                this.ctx.strokeText(i, cell.left + 3, cell.top+22);
+                this.ctx.strokeText(`${cell.row}/${cell.col}`, cell.left + 1, cell.top+10);
+
+            });
 
             setTimeout(this.lifeUpdate, this.delay*1000);
         },
         lifeUpdate() {
             // updates cells .alive property based on the rules of the game.
-            for(let cell of this.grid.array) {
-                const crowd = this.crowdSize(cell);
+
+            this.grid.array.forEach((cell, i) => {
+                cell.crowd = this.crowdSize(cell, i);
 
                 if (cell.alive) {
-                    if (crowd < 2 || crowd > 3) {
+                    if (cell.crowd < 2 || cell.crowd > 3) {
                         cell.nextAlive = false;
                     } else {
                         cell.nextAlive = true;
                     }
                 } else {
-                    if (crowd == 3) {
+                    if (cell.crowd == 3) {
                         cell.nextAlive = true;
                     }
                 }
-            }
+            });
             requestAnimationFrame(this.gridUpdate);
         },
-        crowdSize(cell) {
+        crowdSize(cell, idx) {
             let crowdSize = 0;
-            const idx = cell.col + cell.row*this.gridCount;
+            // const idx = cell.col + cell.row*this.gridCount;
 
-            for (let i = -1; i < 2; i++) {
-                for (let j = -1; j < 2; j++) {
-                    const thisCol = cell.col + j;
-                    const thisRow = cell.row + i;
-                    if (thisCol >= 0 && thisCol < this.gridCount) {
-                        if (thisRow >= 0 && thisRow < this.gridCount) {
-                            if (thisRow != cell.row || thisCol != cell.col) {
-                                const thisIdx = thisCol + thisRow*this.gridCount;
-                                if (this.grid.array[thisIdx].alive) {
-                                    crowdSize++
-                                }
-                            }
-                        }
-                    }
+            // const neighbours = [-26, -25, -24, -1, 1, 24, 25, 26];
+            const neighbours = [];
+
+            if (cell.row > 0) {neighbours.push(-1)};
+            if (cell.col > 0) {neighbours.push(-25)};
+            if (cell.row > 0 && cell.col > 0) {neighbours.push(-26)};
+            if (cell.row < this.gridCount-1 && cell.col > 0) {neighbours.push(-24)};
+            if (cell.row < this.gridCount-1) {neighbours.push(1)};
+            if (cell.row > 0 && cell.col < this.gridCount-1) {neighbours.push(24)};
+            if (cell.col < this.gridCount-1) {neighbours.push(25)};
+            if (cell.row < this.gridCount-1 && cell.col < this.gridCount-1) {neighbours.push(26)};
+
+            // console.log(cell.row);
+            // console.log(cell.col);
+
+            neighbours.forEach((neighbour) => {
+                // console.log(neighbours);
+                // console.log(neighbour);
+                const thisIdx = idx + neighbour;
+                // console.log(`${idx} : ${thisIdx}`);
+                
+                if (this.grid.array[thisIdx].alive) {
+                    crowdSize++;
                 }
-            }
+            })
+
+            // for (let i = -1; i < 2; i++) {
+            //     for (let j = -1; j < 2; j++) {
+            //         const thisCol = cell.col + j;
+            //         const thisRow = cell.row + i;
+            //         if (thisCol >= 0 && thisCol < this.gridCount) {
+            //             if (thisRow >= 0 && thisRow < this.gridCount) {
+            //                 if (thisRow != cell.row || thisCol != cell.col) {
+            //                     const thisIdx = thisCol + thisRow*this.gridCount;
+            //                     if (this.grid.array[thisIdx].alive) {
+            //                         crowdSize++
+            //                     }
+            //                 }
+            //             }
+            //         }
+            //     }
+            // }
             return crowdSize;
         }
     }
