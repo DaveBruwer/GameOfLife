9a158a64276afe441f643b2a7943680b8c642487
Dave Bruwer <dave.bruwer@fphcare.co.nz>
2023-01-23 12:44:36 +1300

    added form field validation to registration form.

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,22 +1,30 @@
 <template>
 <form class="container align-middle" id="loginform">
   <div class="m-3">
-    <label for="displayName" class="form-label">Display Name</label>
-    <input type="text" class="form-control" id="displayName" aria-describedby="dispNameHelp">
-    <div id="dispNameHelp" class="form-text">The name that will be shown to other users.</div>
+    <input v-model="v$.displayName.$model" type="text" class="form-control" id="displayName" aria-describedby="dispNameHelp" placeholder="Enter Display Name">
+    <div v-if="v$.displayName.$errors.length">
+      <div class="text-danger" v-for="error of v$.displayName.$errors" :key="error.$uid">{{ error.$message }}</div>
+    </div>
+    <div v-else id="dispNameHelp" class="form-text">(The name that will be shown to other users.)</div>
   </div>
   <div class="m-3">
-    <label for="RegisterEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="RegisterEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <input v-model="v$.registerEmail.$model" type="email" class="form-control" id="registerEmail" aria-describedby="emailHelp" placeholder="Enter Email">
+    <div v-if="v$.registerEmail.$errors.length">
+      <div class="text-danger" v-for="error of v$.registerEmail.$errors" :key="error.$uid">{{ error.$message }}</div>
+    </div>
+    <div v-else id="emailHelp" class="form-text">(We'll never share your email with anyone else.)</div>
   </div>
   <div class="m-3">
-    <label for="RegisterPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="RegisterPassword1">
+    <input v-model="v$.registerPassword.$model" type="password" class="form-control" id="RegisterPassword1" placeholder="Enter Password">
+    <div v-if="v$.registerPassword.$errors.length">
+      <div class="text-danger" v-for="error of v$.registerPassword.$errors" :key="error.$uid">{{ error.$message }}</div>
+    </div>
   </div>
   <div class="m-3">
-    <label for="ConfirmPassword1" class="form-label">Confirm Password</label>
-    <input type="password" class="form-control" id="ConfirmPassword1">
+    <input v-model="v$.confirmPassword.$model" type="password" class="form-control" id="ConfirmPassword1" placeholder="Confirm Password">
+    <div v-if="v$.confirmPassword.$errors.length">
+      <div class="text-danger" v-for="error of v$.confirmPassword.$errors" :key="error.$uid">{{ error.$message }}</div>
+    </div>
   </div>
   <div class="m-3">
     <RouterLink to="/login">Login</RouterLink>
@@ -26,8 +34,30 @@
 </template>
 
 <script>
+
+import { useVuelidate } from "@vuelidate/core"
+import { required, email, minLength, maxLength, sameAs, helpers} from "@vuelidate/validators"
+
 export default {
-  
+  setup () {
+    return { v$: useVuelidate() }
+  },
+  data() {
+    return {
+      displayName: "",
+      registerEmail: "",
+      registerPassword: "",
+      confirmPassword: ""
+    }
+  },
+  validations() {
+    return {
+      displayName: {required, minLength: minLength(3), maxLength: maxLength(20)},
+      registerEmail: {required, email},
+      registerPassword: {required, minLength: minLength(6)},
+      confirmPassword: {required, sameAs: helpers.withMessage("This field must match the password above.", sameAs(this.registerPassword))}
+    }
+  }
 }
 </script>
 
