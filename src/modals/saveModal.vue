7bf26f319dbe64118d7d05a0f4a06aa955240d4c
DaveBruwer <davebruwer@gmail.com>
2023-03-09 21:54:19 +1300

    disabled submit while saving grid

--- src/modals/saveModal.vue
+++ src/modals/saveModal.vue
@@ -20,7 +20,7 @@
         </div>
         <div class="modal-footer">
           <button @click.prevent="() => {showModal = false}" type="button" class="btn btn-outline-dark" >Cancel</button>
-          <button type="submit" class="btn btn-outline-dark">Save</button>
+          <button type="submit" :disabled="disableSumbit" class="btn btn-outline-dark">Save</button>
         </div>
       </div>
     </div>
@@ -43,7 +43,8 @@
       return {
         saveName: "",
         saveSVG: '<svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" fill="currentColor" class="bi bi-save" viewBox="0 0 16 16"><path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z"/></svg>',
-        showModal: false
+        showModal: false,
+        disableSumbit: false
       }
     },
   props: {
@@ -80,6 +81,7 @@
         const isFormCorrect = await this.v$.$validate()
 
         if (isFormCorrect) {
+          this.disableSumbit = true
           await addDoc(collection(db, "grids"), {
             name: this.saveName,
             count: this.stateStore.count,
@@ -95,6 +97,7 @@
           }).catch((error) => {
             console.log(error.message)
             alert(error.message)
+            this.disableSumbit = false
           })
         } else {
           alert("Invalid form data.")
